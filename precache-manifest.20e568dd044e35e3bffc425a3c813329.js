e46639df89bec9725368472cda69727c7899eb62
ovichowdhury <ovi.chowdhury@hotmail.com>
2020-01-22 21:33:47 +0600

    Release 1.0

---
+++ precache-manifest.20e568dd044e35e3bffc425a3c813329.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a06eaa273dbf6466c5ca0c69e4bd1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8de38c063ccbcf445b9c",
+    "url": "/static/css/2.0f801562.chunk.css"
+  },
+  {
+    "revision": "414518ff9d9c1e78b62a",
+    "url": "/static/css/main.c2fea502.chunk.css"
+  },
+  {
+    "revision": "8de38c063ccbcf445b9c",
+    "url": "/static/js/2.1f2ac271.chunk.js"
+  },
+  {
+    "revision": "414518ff9d9c1e78b62a",
+    "url": "/static/js/main.b50ef279.chunk.js"
+  },
+  {
+    "revision": "ee6302198a2f447164af",
+    "url": "/static/js/runtime-main.1cc7024b.js"
+  },
+  {
+    "revision": "296e220fc291ae028e4a4e41ef461252",
+    "url": "/static/media/border.296e220f.png"
+  },
+  {
+    "revision": "74250345a8158a206e603404ccb4d6c4",
+    "url": "/static/media/logo1.74250345.png"
+  },
+  {
+    "revision": "86b5ca086eab927fe7b54cad9ca74ee5",
+    "url": "/static/media/mehedi1.86b5ca08.jpg"
+  },
+  {
+    "revision": "ca37dc94521e5b43811e5ea7892b989e",
+    "url": "/static/media/monir.ca37dc94.jpeg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "326ba54ea3c24cc4dba8a646e3ec0483",
+    "url": "/static/media/ovipass.326ba54e.jpg"
+  }
+]);
